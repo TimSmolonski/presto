c4364d9297a243726a59afcf1726a090497ef912
Tim Smolonski <programeezy@gmail.com>
2018-11-15 12:29:12 +0300

    Add dishes and toppings endpoints

--- presto_api/urls.py
+++ presto_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import RestaurantViewSet
-
+from .views import RestaurantViewSet, DishViewSet, ToppingViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'restaurants', RestaurantViewSet, base_name='restaurants')
+router.register(r'dishes', DishViewSet, base_name='dishes')
+router.register(r'toppings', ToppingViewSet, base_name='toppings')
 
 urlpatterns = [
     path('', include(router.urls))
--- presto_api/views.py
+++ presto_api/views.py
@@ -7,3 +7,13 @@ from .serializers import RestaurantSerializer, DishSerializer, ToppingSerializer
 class RestaurantViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+
+
+class DishViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
+
+
+class ToppingViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Topping.objects.all()
+    serializer_class = ToppingSerializer
